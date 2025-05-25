4ae0586f93e131d29f8020c7107238850c688ce6
rxyxxy <80524108+rxyxxy@users.noreply.github.com>
2025-05-25 17:42:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,15 @@
-wget https://builds.dotnet.microsoft.com/dotnet/Runtime/5.0.16/dotnet-runtime-5.0.16-linux-x64.tar.gz -O dotnet-runtime-5.0.16.tar.gz
-mkdir -p $HOME/dotnet5
-tar -xzf dotnet-runtime-5.0.16.tar.gz -C $HOME/dotnet5
-export DOTNET_ROOT=$HOME/dotnet5
-export PATH=$DOTNET_ROOT:$PATH
+if command -v dotnet >/dev/null 2>&1; then
+    echo "dotnet 已安装"
+    dotnet Cli.dll start accept --token $token 
+else
+    echo "dotnet 未安装"   
+    wget https://builds.dotnet.microsoft.com/dotnet/Runtime/5.0.16/dotnet-runtime-5.0.16-linux-x64.tar.gz -O dotnet-runtime-5.0.16.tar.gz
+    mkdir -p $HOME/dotnet5
+    tar -xzf dotnet-runtime-5.0.16.tar.gz -C $HOME/dotnet5
+    export DOTNET_ROOT=$HOME/dotnet5
+    export PATH=$DOTNET_ROOT:$PATH
+    dotnet Cli.dll start accept --token $token
+fi
 
-dotnet Cli.dll start accept --token $token
+
+
